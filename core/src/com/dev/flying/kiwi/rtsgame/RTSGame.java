74484939ff6f980eecac846f67706adad65088da
Steven Rutherford <steve.rutherford12@gmail.com>
2015-09-09 20:22:27 +1200

    Add movement system to engine

--- core/src/com/dev/flying/kiwi/rtsgame/RTSGame.java
+++ core/src/com/dev/flying/kiwi/rtsgame/RTSGame.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dev.flying.kiwi.rtsgame.core.World;
 import com.dev.flying.kiwi.rtsgame.systems.ImageRenderSystem;
+import com.dev.flying.kiwi.rtsgame.systems.MovementSystem;
 
 public class RTSGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -25,6 +26,7 @@ public class RTSGame extends ApplicationAdapter {
 		this.engine = new PooledEngine();
 
 		this.engine.addSystem(new ImageRenderSystem(batch));
+		this.engine.addSystem(new MovementSystem());
 		this.world = new World(this.engine);
 		this.world.create();
 	}
