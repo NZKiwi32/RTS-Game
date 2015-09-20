edd04ac7320becb873b38a9f38f17bdb81d6f37e
Steven Rutherford <steve.rutherford12@gmail.com>
2015-09-20 17:33:14 +1200

    Basics of a spawning system, spawns united at a specific location at a given interval

---
+++ core/src/com/dev/flying/kiwi/rtsgame/components/SpawnerComponent.java
@@ -0,0 +1,22 @@
+package com.dev.flying.kiwi.rtsgame.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * A component for 2D PositionComponent
+ * Created by Steven on 7/29/2015.
+ */
+public class SpawnerComponent implements Component {
+    public float x;
+    public float y;
+
+    public SpawnerComponent(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return SpawnerComponent.class.getSimpleName() + "=(" + this.x + "," + this.y + ")";
+    }
+}
+
