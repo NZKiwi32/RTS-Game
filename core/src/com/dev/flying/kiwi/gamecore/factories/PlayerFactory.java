b8986ca8ce9ec7a48968858abfd0adf28e28886f
Steven Rutherford <steve.rutherford12@gmail.com>
2015-09-23 15:28:08 +1200

    Created player body, added to world and abstracted creation into a factory class.

---
+++ core/src/com/dev/flying/kiwi/gamecore/factories/PlayerFactory.java
@@ -0,0 +1,38 @@
+package com.dev.flying.kiwi.gamecore.factories;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+/**
+ * Created by Steven on 9/23/2015.
+ */
+public class PlayerFactory {
+    public static Body createPlayer(World world, float x, float y) {
+        BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x, y);
+
+        Body player = world.createBody(bodyDef);
+
+        CircleShape ballShape = new CircleShape();
+        ballShape.setRadius(1f);
+
+        // fixture definition
+        fixtureDef.shape = ballShape;
+        fixtureDef.density = 2.5f;
+        fixtureDef.friction = .25f;
+        fixtureDef.restitution = .75f;
+
+        player.createFixture(fixtureDef);
+
+        ballShape.dispose();
+
+        return player;
+    }
+
+    public static Body createPlayer(World world) {
+        return PlayerFactory.createPlayer(world,0,0);
+    }
+}
--- core/src/com/dev/flying/kiwi/gamecore/screens/PlayScreen.java
+++ core/src/com/dev/flying/kiwi/gamecore/screens/PlayScreen.java
@@ -4,42 +4,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.dev.flying.kiwi.gamecore.factories.PlayerFactory;
 
 /**
  * A Screen which contains the stage and all actors. This controls the main part of the game.
  * Created by Steven on 9/21/2015.
  */
 public class PlayScreen implements Screen {
+    private final float TIMESTEP = 1 / 60f;
+    private final int VELOCITYITERATIONS = 8, POSITIONITERATIONS = 3;
+
     private Color background;
-    private boolean loaded; // Is the level finished loading
 
+    private OrthographicCamera camera;
     private Stage stage;
-
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
 
     @Override
     public void show() {
-        background = new Color(0.3f, 0.7f, 0.95f, 1f);
+        background = new Color(0.3f, 0.2f, 0.25f, 1f);
 
         stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
-
         Gdx.input.setInputProcessor(stage);
 
-        createPlayer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth() / 25, Gdx.graphics.getHeight() / 25);
 
-        loaded = true;
+        world = new World(new Vector2(0,0), true);
+        debugRenderer = new Box2DDebugRenderer();
+        PlayerFactory.createPlayer(world);
     }
 
 
-    private void createPlayer() {
-
-    }
 
     private void update(float delta) {
-        if(loaded) {
-            stage.act(delta);
-        }
     }
 
 
@@ -47,7 +50,8 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(background.r, background.g, background.b, background.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stage.draw();
+        world.step(TIMESTEP, VELOCITYITERATIONS, POSITIONITERATIONS);
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
@@ -58,7 +62,8 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
+        camera.viewportWidth = width / 25;
+        camera.viewportHeight = height / 25;
     }
 
     @Override
@@ -73,11 +78,13 @@ public class PlayScreen implements Screen {
 
     @Override
     public void hide() {
-
+        dispose();
     }
 
     @Override
     public void dispose() {
+        world.dispose();
+        debugRenderer.dispose();
         stage.dispose();
     }
 }
